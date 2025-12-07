a12b18155bf01973eaecbf579411578f577563e4
Fahim2023 <fahimshahrier2023@gmail.com>
2025-12-07 11:35:13 +0600

    implented tancstack query in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,17 @@ import App from "./App.jsx";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./Routes/Routes.jsx";
 import AuthProvider from "./Context/AuthProvider.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider queryClient={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
